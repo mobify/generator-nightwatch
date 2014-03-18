8d589f9ffa672face87c8c1ed5172ef82b69b295
Keith Bingman <keith@mobify.com>
2014-03-18 10:36:47 -0700

    added prompt for URL and Bundle URL

--- app/index.js
+++ app/index.js
@@ -1,28 +1,64 @@
 'use strict';
+
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 
-
 var NightwatchGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
+    init: function () {
+        this.pkg = yeoman.file.readJSON(path.join(__dirname, '../package.json'));
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.npmInstall();
-      }
-    });
-  },
+        this.on('end', function () {
+            if (!this.options['skip-install']) {
+                this.npmInstall();
+            }
+        });
+    },
 
-  askFor: function () {
+
+
+    projectfiles: function () {
+    }
+});
+
+NightwatchGenerator.prototype.askFor = function askFor() {
+    var cb = this.async();
+
+    // Have yeoman greet the user
     console.log(this.yeoman);
 
-    console.log(chalk.magenta('Generating Nightwatch integration test infrastructure.'));
-  },
+    var prompts = [
+        {
+            type: 'input',
+            name: 'siteUrl',
+            message: 'What is the URL of your project?',
+            validate: function (answer) {
+                if (!answer.length) {
+                    return 'You must enter a URL.';
+                }
+                return true;
+            }
+        },{
+            type: 'input',
+            name: 'bundleUrl',
+            message: 'What is the URL of your Mobify bundle?',
+            default: 'http://localhost:8080'
+        },
+    ]
 
-  app: function () {
+    this.prompt(prompts, function (props) {
+        this.siteUrl = props.siteUrl;
+        this.bundleUrl = props.bundleUrl;
+
+        console.log(chalk.magenta('Generating Nightwatch integration test infrastructure.'));
+
+        cb();
+    }.bind(this));
+
+};
+
+NightwatchGenerator.prototype.app = function app() {
     this.mkdir('tests');
 
     this.copy('package.json', 'package.json');
@@ -30,10 +66,6 @@ var NightwatchGenerator = yeoman.generators.Base.extend({
 
     this.directory('nightwatch', 'tests/integration');
     this.template('_site.json', 'tests/integration/site.json');
-  },
-
-  projectfiles: function () {
-  }
-});
+};
 
 module.exports = NightwatchGenerator;
