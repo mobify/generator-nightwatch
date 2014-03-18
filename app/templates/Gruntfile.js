5f1a646981ab992fdfb5296cf4b207c46d239e0d
Keith Bingman <keith@mobify.com>
2014-03-18 10:41:06 -0700

    updated grunt task

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -1,23 +1,21 @@
 
 module.exports = function(grunt) {
-  var child;
+    var child;
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-  });
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+    });
 
-  grunt.registerTask('nightwatch', function () {
-    var callback = this.async();
+    grunt.registerTask('nightwatch', function () {
+        var callback = this.async();
 
-    child && child.kill();
-
-    child = grunt.util.spawn({
-        cmd: 'node',
-        args: ['node_modules/nightwatch/bin/runner.js','-c','tests/integration/nightwatch.json'],
-        opts: {stdio: 'inherit'}
-      },
-      function() {
-        callback();
-      });
-  });
+        grunt.util.spawn({
+            cmd: 'node',
+            args: [].concat(['node_modules/nightwatch/bin/runner.js', '-c', 'tests/integration/nightwatch.json'], grunt.option.flags()),
+            opts: {stdio: 'inherit'}
+        },
+        function() {
+            callback();
+        });
+    });
 };
